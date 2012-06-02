9dcb999f70b3a67d1f42a0d873cfcfbae1a90da8
Bill DePhillips <bill.dephillips@gmail.com>
2012-06-02 15:39:38 -0700

    markdown mode working

--- .emacs.d/.emacs
+++
@@ -1,1 +0,0 @@
-(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t) (setq auto-mode-alist (cons '("\\.text" . markdown-mode) auto-mode-alist))
---
+++ .emacs.d/init.el
@@ -0,0 +1,3 @@
+(add-to-list 'load-path "~/.emacs.d/markdown-mode/")
+
+(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t) (setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
