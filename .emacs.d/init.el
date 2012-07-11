6c5b0ec328766a93cdd618a59c6dbd3e06dc5e93
Bill DePhillips <bill.dephillips@gmail.com>
2012-07-10 21:55:05 -0700

    added some rails helpers

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,17 @@
 (add-to-list 'load-path "~/.emacs.d/markdown-mode/")
 
+(load "~/.emacs.d/nxhtml/autostart.el")
+
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t) (setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
