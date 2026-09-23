f6fc6c9925d15b84c170d4112f34a420b1b3d25d
Bill DePhillips <355438+vegetabill@users.noreply.github.com>
2026-09-23 10:37:42 -0700

    updated install.sh

--- install.sh
+++ install.sh
@@ -2,14 +2,16 @@
 
 echo "this assumes you checked out this repo into ~/dotfiles"
 
-
 # Just showing off that cd with no args goes home 🤪
 cd
 
 for f in .profile .zshrc .tmux.conf .vimrc .gitconfig .gitignore
 do
+    mv $f ${f}.pre-dotfiles.bak
     rm $f
     ln -s ~/dotfiles/${f} ${f}
 done
+
+mkdir -p ~/.config
 
 echo "✅✅✅✅✅✅✅✅✅✅✅"
